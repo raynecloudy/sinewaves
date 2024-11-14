259bc399ccd75e1956a12041d78cc47c143a3779
hayley <qwertyytheartist@gmail.com>
2024-11-13 17:09:05 -0800

    sine wave ^w^

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::*;
 
 fn main() {
-    let (w, h) = termion::terminal_size().unwrap();
+    let (w, _) = termion::terminal_size().unwrap();
     println!("welcome to sine wave generator");
     println!("y = a * sin(bx)");
     let mut a = String::new();
@@ -16,16 +16,34 @@ fn main() {
         Ok(_) => print!(""),
         Err(err) => println!("there was an error: {}", err)
     }
-    let a = a.trim().parse::<i32>().unwrap();
-    let b = b.trim().parse::<i32>().unwrap();
-    for y in -a..a+1 {
+    let a = a.trim().parse::<f32>().unwrap();
+    let b = b.trim().parse::<f32>().unwrap();
+    for y in -a.floor() as i32..a.ceil() as i32+1 {
         print!("\x1b[38;2;255;122;200;48;2;42;37;46m{:4}│", -y);
-        for x in 0..(w/2-5) {
-            if ((y.abs() % 2 == 0 && x % 2 == 0) || (y.abs() % 2 == 1 && x % 2 == 1)) {
+        for x in 0..w-5 {
+            print!("\x1b[0;38;2;255;205;125;48;2;42;37;46m");
+            if (y.abs() % 2 == 0 && x/2 % 2 == 0) || (y.abs() % 2 == 1 && x/2 % 2 == 1) {
                 print!("\x1b[48;2;54;48;59m");
             }
-            print!("  \x1b[0m\x1b[48;2;42;37;46m");
+            if -y as f32 == (a * (b * (x as f32) / 2.0).to_radians().sin()).floor() {
+                let point = (a * (b * (x as f32) / 2.0).to_radians().sin()) - (a * (b * (x as f32) / 2.0).to_radians().sin()).floor();
+                if point < 0.1 {
+                    print!("_");
+                } else if point < 0.2 {
+                    print!(",");
+                } else if point < 0.4 {
+                    print!(".");
+                } else if point < 0.6 {
+                    print!("-");
+                } else if point < 0.8 {
+                    print!("'");
+                } else {
+                    print!("`");
+                }
+            } else {
+                print!(" ");
+            }
         }
-        println!();
+        println!("\x1b[0m");
     }
 }
