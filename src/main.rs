d1d00e05c20ee3fec0b42b693229df5816d885b7
hayley <qwertyytheartist@gmail.com>
2024-11-13 16:18:42 -0800

    graph

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::*;
 
 fn main() {
+    let (w, h) = termion::terminal_size().unwrap();
     println!("welcome to sine wave generator");
     println!("y = a * sin(bx)");
     let mut a = String::new();
@@ -18,6 +19,13 @@ fn main() {
     let a = a.trim().parse::<i32>().unwrap();
     let b = b.trim().parse::<i32>().unwrap();
     for y in -a..a+1 {
-        println!("{}\x1b[10D\x1b[4C│", -y);
+        print!("\x1b[38;2;255;122;200;48;2;42;37;46m{:4}│", -y);
+        for x in 0..(w/2-5) {
+            if ((y.abs() % 2 == 0 && x % 2 == 0) || (y.abs() % 2 == 1 && x % 2 == 1)) {
+                print!("\x1b[48;2;54;48;59m");
+            }
+            print!("  \x1b[0m\x1b[48;2;42;37;46m");
+        }
+        println!();
     }
 }
